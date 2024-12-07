7477b9f39f4209b8771868f17e762b025236ef6c
vinoth <mohanrajvinoth2004@gmail.com>
2024-12-07 12:26:56 +0530

    replaced events to current track

--- src/Components/NonCircuitnTech.jsx
+++ src/Components/NonCircuitnTech.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import randomColor from "randomcolor";
 import "../text.css";
 import Card from "./Card";
-<<<<<<< HEAD
 import Image from "../utils/Img/TechLogo.jpg";
 const NonCircuitnTech = () => {
   const color = randomColor();
@@ -63,70 +62,6 @@ const NonCircuitnTech = () => {
             />
           ))}
         </main>
-=======
-import Image from '../utils/Img/TechLogo.jpg';
-import RandomEvents from "./RandomEvents";
-const NonCircuitnTech = () => {
-    const color = randomColor();
-    const noncircuitntech = RandomEvents("Circuit Branch - Technical Events")
-    // Events data
-    const circuittechnicalEvents = [
-      {
-        id: 1,
-        imageSrc: Image, // Replace with actual image path
-        altText: "Cinephilia",
-        label: "Cinephilia",
-        onClick: () => alert("Cinephilia clicked"),
-      },
-      {
-        id: 2,
-        imageSrc: Image, // Replace with actual image path
-        altText: "Chronojive",
-        label: "Chronojive",
-        onClick: () => alert("Chronojive clicked"),
-      },
-      {
-        id: 3,
-        imageSrc: Image, // Replace with actual image path
-        altText: "BGMI",
-        label: "BGMI",
-        onClick: () => alert("BGMI clicked"),
-      },
-      {
-        id: 4,
-        imageSrc: Image, // Replace with actual image path
-        altText: "Free Fire",
-        label: "Free Fire",
-        onClick: () => alert("Free Fire clicked"),
-      },
-    ];
-  
-    return (
-      <div className="max-w-7xl mx-auto p-6">
-        <h3
-          className="text-3xl md:text-4xl font-bold text-center mb-8 text-white uppercase event-text tracking-widest"
-          style={{
-            textShadow: `2px 2px 15px ${color}`,
-          }}
-        >
-          Non-Technical Events
-        </h3>
-  
-        <div>
-          <main className="card-container gap-10 relative flex flex-wrap justify-center w-full">
-            {/* Loop through each event and render a Card */}
-            {circuittechnicalEvents.map((card) => (
-              <Card
-                key={card.id}
-                imageSrc={card.imageSrc}
-                altText={card.altText}
-                label={card.label}
-                onClick={card.onClick}
-              />
-            ))}
-          </main>
-        </div>
->>>>>>> 76f99aee99a50b10a05c98b20adc4fda317fc242
       </div>
     </div>
   );
