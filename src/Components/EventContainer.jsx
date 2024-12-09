7800af0da52f06b518558196edda81ed3bdb9f15
shifan-mts <amohamedshifan1@gmail.com>
2024-12-09 18:44:19 +0530

    added margin bottom of the buttons(track I and II)

--- src/Components/EventContainer.jsx
+++ src/Components/EventContainer.jsx
@@ -10,7 +10,7 @@ const EventContainer = () => {
       <div className="flex justify-center mt-11 mb-20">
           <img src={eventPoster} alt="Event Poster" />
       </div>
-      <div className="lg:gap-10 gap-5 flex flex-col sm:flex-row justify-center items-center animate__animated animate__fadeInUp">
+      <div className="lg:gap-10 gap-5 mb-10 flex flex-col sm:flex-row justify-center items-center animate__animated animate__fadeInUp">
         <Link to="/events/non-circuit">
           <NeonButton label="Track I" tooltip={"AIDS/AIML/CS/CSE/CSBS/IT"} className="bg-red-500" />
         </Link>
