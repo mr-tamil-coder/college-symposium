eca8a2e2eea31982f33b34ab0498df32e8f6d5b9
vinoth <mohanrajvinoth2004@gmail.com>
2024-12-09 18:49:27 +0530

    minimized poster

--- src/Components/EventContainer.jsx
+++ src/Components/EventContainer.jsx
@@ -3,16 +3,20 @@ import Circuit from "./Circuit";
 import NonCircuit from "./NonCircuit";
 import NeonButton from "./NeonButton";
 import { Link } from "react-router-dom";
-import eventPoster from "../assets/symposium/eventPoster.jpg"
+import eventPoster from "../assets/symposium/eventPoster.jpg";
 const EventContainer = () => {
   return (
     <div>
       <div className="flex justify-center mt-11 mb-20">
-          <img src={eventPoster} alt="Event Poster" />
+        <img src={eventPoster} className="h-[460px]" alt="Event Poster" />
       </div>
       <div className="lg:gap-10 gap-5 flex flex-col sm:flex-row justify-center items-center animate__animated animate__fadeInUp">
         <Link to="/events/non-circuit">
-          <NeonButton label="Track I" tooltip={"AIDS/AIML/CS/CSE/CSBS/IT"} className="bg-red-500" />
+          <NeonButton
+            label="Track I"
+            tooltip={"AIDS/AIML/CS/CSE/CSBS/IT"}
+            className="bg-red-500"
+          />
         </Link>
         <Link to="/events/circuit">
           <NeonButton label="Track II" tooltip={"BME/CE/ECE/EEE/MBA"} />
